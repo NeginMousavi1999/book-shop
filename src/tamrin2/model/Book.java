212fd299d4ec46ae7f226cf6313f430fc44b385d
negin <negin.s.mousavi@gmail.com>
2021-11-06 20:47:17 +0330

    create scale of book class

--- src/tamrin2/model/Book.java
+++ src/tamrin2/model/Book.java
@@ -1,7 +1,29 @@
 package tamrin2.model;
 
+import lombok.Data;
+
 /**
  * @author Negin Mousavi
  */
+@Data
 public class Book {
+    private int id;
+    private String isbn;
+    private String authorName;
+    private String title;
+    private int publishedYear;
+    private double price;
+    private int soledNumber;
+    private double soledTotalPrice;
+
+    public Book(int id, String isbn, String authorName, String title, int publishedYear, double price, int soledNumber) {
+        this.id = id;
+        this.isbn = isbn;
+        this.authorName = authorName;
+        this.title = title;
+        this.publishedYear = publishedYear;
+        this.price = price;
+        this.soledNumber = soledNumber;
+        this.soledTotalPrice = soledNumber * price;
+    }
 }
