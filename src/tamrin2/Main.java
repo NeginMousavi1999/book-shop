18e1243ae500f09df56613e30ea2e07d5ddc14ff
negin <negin.s.mousavi@gmail.com>
2021-11-06 21:13:06 +0330

    add main method to test if get all names is correct

--- src/tamrin2/Main.java
+++ src/tamrin2/Main.java
@@ -1,7 +1,27 @@
 package tamrin2;
 
+import tamrin2.service.BookService;
+
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  * @author Negin Mousavi
  */
 public class Main {
+    //    Scanner scanner = new Scanner(System.in);
+    static BookService bookService;
+
+    static {
+        try {
+            bookService = new BookService();
+        } catch (SQLException | ClassNotFoundException throwable) {
+            throwable.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        List<String> names = bookService.getAllAuthorsName();
+        names.forEach(System.out::println);
+    }
 }
